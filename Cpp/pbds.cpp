4bbc9cc1e7f4789b5c34a84b4a79bb2e46ae474d
Bhavishya tiwari <85561955+Bhavishya-tiwari@users.noreply.github.com>
2022-10-18 22:02:44 +0530

    Update pbds.cpp

--- Cpp/pbds.cpp
+++ Cpp/pbds.cpp
@@ -1,57 +1,22 @@
-#include <bits/stdc++.h>
-#include <cmath>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <limits.h>
-
-// pbds
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template<class T> using oset = tree<T, null_type, less<T>,
-                         rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using oset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// less: it is the basic for comparison of two function. Use less_equal for ordered multiset.
+// order_of_key(k): Number of items strictly smaller than k
+// find_by_order(k): kth element in a set (counting from zero) 
 
-
-using namespace std;
-#define ll long long int
-#define pb(x) push_back(x)
-#define ff first
-#define ss second
-#define uu unsigned long long int
-#define fio                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-
-
-
-void solve_kr_be()
-{
-    // oset<pair<int,int>> st;
-    // oset ki jagah kuch bhi name rakh skte hai
-    // because of template class we used we can create oset of any data type
-    oset<int> st;
-    st.insert(1);
-    st.insert(2);
-    st.insert(3);
-
-    // cout kth element
-    // cout<<*s.find_by_order(k);
-
-    // cout number of elements smaller then t
-    // cout<<*s.order_of_key(t);
-
-}
-int main()
-{
-    // fio
-    ll TestCases;
-    cin >> TestCases;
-    while (TestCases--)
-    {
-        solve_kr_be();
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        oset<int> st;
+        vector<int> ans;
+        int n = nums.size();
+        for(int i=n-1; i>=0; --i){
+            ans.push_back(st.order_of_key(nums[i]));
+            st.insert(nums[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-
-    return 0;
-}
+};
